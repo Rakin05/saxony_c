4ebf89c9d9d888a05a552d90669bd5de8c28381d
Felix Klotzsche <felixklotzsche@gmail.com>
2014-04-23 11:39:16 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "saxony_c.h"
+
+ganse_zohl main(ganse_zohl argc, buchstahm_pungter argv[])
+{
+  schreibe_ma("Gudn Daach, Weld\n");
+  gibbe_zurigg ferdsch;
+}
---
+++ saxony_c.h
@@ -0,0 +1,12 @@
+#ifndef SAXONY_C_H
+#define SAXONY_C_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#define schreibe_ma printf
+#define ganse_zohl int
+#define buchstahm_pungter char*
+#define gibbe_zurigg return
+#define ferdsch EXIT_SUCCESS
+
+#endif
